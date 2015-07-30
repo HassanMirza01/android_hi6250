cbe269f13b3680b927c2491d35d43b5f9575cc0b
Viresh Kumar <viresh.kumar@linaro.org>
2015-07-30 12:40:30 +0530

    thermal/cpu_cooling: No need to initialize max_freq to 0
    Its always set before getting used, don't initialize it.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Eduardo Valentin <edubezval@gmail.com>

--- drivers/thermal/cpu_cooling.c
+++ drivers/thermal/cpu_cooling.c
@@ -234,7 +234,7 @@ static int cpufreq_thermal_notifier(struct notifier_block *nb,
 				    unsigned long event, void *data)
 {
 	struct cpufreq_policy *policy = data;
-	unsigned long max_freq = 0;
+	unsigned long max_freq;
 	struct cpufreq_cooling_device *cpufreq_dev;
 #ifdef CONFIG_HISI_THERMAL_SPM
 	enum ipa_actor actor;
