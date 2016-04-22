8a6ff54ed1393cc457ad17e79945508d2f2fba16
Daniel Rosenberg <drosen@google.com>
2016-04-22 00:00:14 -0700

    vfs: change d_canonical_path to take two paths
    bug: 23904372
    Change-Id: I4a686d64b6de37decf60019be1718e1d820193e6
    Signed-off-by: Daniel Rosenberg <drosen@google.com>

--- fs/sdcardfs/dentry.c
+++ fs/sdcardfs/dentry.c
@@ -46,7 +46,8 @@ static int sdcardfs_d_revalidate(struct dentry *dentry, unsigned int flags)
 	spin_unlock(&dentry->d_lock);
 
 	/* check uninitialized obb_dentry and
-	 * whether the base obbpath has been changed or not */
+	 * whether the base obbpath has been changed or not
+	 */
 	if (is_obbpath_invalid(dentry)) {
 		d_drop(dentry);
 		return 0;
@@ -116,21 +117,14 @@ out:
 static void sdcardfs_d_release(struct dentry *dentry)
 {
 	/* release and reset the lower paths */
-	if (has_graft_path(dentry)) {
+	if (has_graft_path(dentry))
 		sdcardfs_put_reset_orig_path(dentry);
-	}
 	sdcardfs_put_reset_lower_path(dentry);
 	free_dentry_private_data(dentry);
-	return;
 }
 
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 11, 0))
 static int sdcardfs_hash_ci(const struct dentry *dentry,
 				struct qstr *qstr)
-#else
-static int sdcardfs_hash_ci(const struct dentry *dentry,
-		const struct inode *inode, struct qstr *qstr)
-#endif
 {
 	/*
 	 * This function is copy of vfat_hashi.
@@ -156,16 +150,9 @@ static int sdcardfs_hash_ci(const struct dentry *dentry,
 /*
  * Case insensitive compare of two vfat names.
  */
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3, 11, 0))
 static int sdcardfs_cmp_ci(const struct dentry *parent,
 		const struct dentry *dentry,
 		unsigned int len, const char *str, const struct qstr *name)
-#else
-static int sdcardfs_cmp_ci(const struct dentry *parent,
-		const struct inode *pinode,
-		const struct dentry *dentry, const struct inode *inode,
-		unsigned int len, const char *str, const struct qstr *name)
-#endif
 {
 	/* A filename cannot end in '.' or we treat it like it has none */
 	/*
@@ -183,9 +170,17 @@ static int sdcardfs_cmp_ci(const struct dentry *parent,
 	return 1;
 }
 
+static void sdcardfs_canonical_path(const struct path *path,
+				struct path *actual_path)
+{
+	sdcardfs_get_real_lower(path->dentry, actual_path);
+}
+
 const struct dentry_operations sdcardfs_ci_dops = {
 	.d_revalidate	= sdcardfs_d_revalidate,
 	.d_release	= sdcardfs_d_release,
 	.d_hash	= sdcardfs_hash_ci,
 	.d_compare	= sdcardfs_cmp_ci,
+	.d_canonical_path = sdcardfs_canonical_path,
 };
+
