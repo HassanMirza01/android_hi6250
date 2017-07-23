a4e91d1a18646908ed722df9d1a3f3c9852f7981
Huawei-Dev <surdu_petru@yahoo.com>
2017-07-23 09:42:56 +0200

    hi6250: disable Huawei bootfail solution

--- drivers/hisi/mntn/blackbox/rdr_core.c
+++ drivers/hisi/mntn/blackbox/rdr_core.c
@@ -465,7 +465,7 @@ bool rdr_init_done()
 static s32 __init rdr_init(void)
 {
 	struct task_struct *rdr_main = NULL;
-	struct task_struct *rdr_bootcheck = NULL;
+	//struct task_struct *rdr_bootcheck = NULL;
 	struct sched_param   param;
 
 	BB_PRINT_START();
@@ -487,7 +487,7 @@ static s32 __init rdr_init(void)
 		kthread_stop(rdr_main);
 		wake_lock_destroy(&blackbox_wl);
 		return -1;
-	}
+	}/*
 	rdr_bootcheck =
 	    kthread_run(rdr_bootcheck_thread_body, NULL, "bbox_bootcheck");
 	if (!rdr_bootcheck) {
@@ -495,7 +495,7 @@ static s32 __init rdr_init(void)
 		kthread_stop(rdr_main);
 		wake_lock_destroy(&blackbox_wl);
 		return -1;
-	}
+	}*/
 	if (!kthread_run(rdr_dump_init, NULL, "bbox_dump_init")) {
 		BB_PRINT_ERR("create thread rdr_dump_init faild.\n");
 		kthread_stop(rdr_main);
